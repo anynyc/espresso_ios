acc879e6705a19de915ffb275c8eff529986dfd7
Michael S Trumpf <miketrumpf@gmail.com>
2017-02-10 16:01:40 -0500

    initial commit

---
+++ espressoV1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  espressoV1
+//
+//  Created by Mike on 2/10/17.
+//  Copyright © 2017 GoodGood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
